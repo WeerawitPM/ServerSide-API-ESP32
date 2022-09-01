857cf962917744b375f21191a1fead9a9c3e7006
WeerawitPM <63030079@kmitl.ac.th>
2022-09-02 00:20:04 +0700

    edit api

--- index.js
+++ index.js
@@ -30,14 +30,12 @@ const GetData = async(req,res)=>{
 const AddData = async(req,res)=>{
     let {Temperature_C2,Humadity2} = req.body
     let Temperature_F = 1.8*Temperature_C2+32
-    let MaxTemperature_C = 50
-    let MaxHumadity = 100
     var Temperature_C = Number(Temperature_C2)
     var Humadity = Number(Humadity2)
     try {
         // console.log(typeof Temperature_C)
         // console.log(typeof Humadity)
-        const data = new ValuesModel({Temperature_C,Temperature_F,Humadity,MaxTemperature_C,MaxHumadity})
+        const data = new ValuesModel({Temperature_C,Temperature_F,Humadity})
         await data.save()
         res.json({Message:"Data Added Success",data})
     }catch (error) {res.json({Message:"Error",error})}
@@ -47,46 +45,20 @@ const AddData_Query = async(req,res)=>{
     let Temperature_C2 = req.query.temp
     let Humadity2 = req.query.hum
     let Temperature_F = 1.8*Temperature_C2+32
-    let MaxTemperature_C = 50
-    let MaxHumadity = 100
     var Temperature_C = Number(Temperature_C2)
     var Humadity = Number(Humadity2)
     try {
         // console.log(typeof Temperature_C)
         // console.log(typeof Humadity)
-        const data = new ValuesModel({Temperature_C,Temperature_F,Humadity,MaxTemperature_C,MaxHumadity})
+        const data = new ValuesModel({Temperature_C,Temperature_F,Humadity})
         await data.save()
         res.json({Message:"Temp Added Success",data})
     }catch (error) {res.json({Message:"Error",error})}
-}
-
-let led =0
-const Toggle = (req,res)=>{
-    try {
-        if(led==0){
-            led=1
-        }
-        else{
-            led=0
-        }
-        res.json({Message:"Done",led})
-    } catch (error) {
-        res.json({Message:"Error",error})
-    }
-}
-const Led = (req,res)=>{
-    try {
-        res.json(led)
-    } catch (error) {
-        res.json({Message:"Error",error})
-    }
 }
 
 //......................... APIs ........................
 app.get("/",GetData)
 app.post("/",AddData)  // Adding data through post metheod & body
 app.get("/add",AddData_Query) // Adding data through get method & query
-app.get("/to",Toggle)
-app.get("/led",Led)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
