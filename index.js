b2e65a32a3ba7825e42bf95df784ae1e1e6a1a75
WeerawitPM <63030079@kmitl.ac.th>
2022-09-13 12:02:14 +0700

    update:schema and add_data_query

--- index.js
+++ index.js
@@ -2,89 +2,100 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ValuesModel = require('./models/models');
 const app = express()
-require ('dotenv').config();
+require('dotenv').config();
 const port = process.env.PORT || 5000
 
 app.use(express.json())
 
 // .................... DB Config .......................
-const DBConnection = ()=>{
-    return mongoose.connect(process.env.CONNECTION_STRING,{
+const DBConnection = () => {
+    return mongoose.connect(process.env.CONNECTION_STRING, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then((result)=>{console.log("DB CONNECTED SUCCESSFULLY")})
-    .catch((error)=>{console.log(error)})
+        .then((result) => { console.log("DB CONNECTED SUCCESSFULLY") })
+        .catch((error) => { console.log(error) })
 }
 DBConnection()
 
 //................... Controllers .........................
-const GetData = async(req,res)=>{
+const GetData = async (req, res) => {
     try {
         const data = await ValuesModel.find()
         // res.json({Message:"All Data",data})
         res.json(data)
-    }catch (error) { res.json({Message:"Error",error})}
+    } catch (error) { res.json({ Message: "Error", error }) }
 }
 
-const FindData = async(req,res)=>{
+const FindData = async (req, res) => {
     let _id = req.params.id
     try {
         const data = await ValuesModel.findById(_id)
-        res.json({Message:"Data Found",data})
-    }catch (error) { res.json({Message:"Error",error})}
+        res.json({ Message: "Data Found", data })
+    } catch (error) { res.json({ Message: "Error", error }) }
 }
 
-const AddData = async(req,res)=>{
-    let {Temperature_C, Temperature_F, Humadity,} = req.body
+const AddData = async (req, res) => {
+    let { Temperature_C, Temperature_F, Humadity, } = req.body
 
     try {
         // console.log(typeof Temperature_C)
         // console.log(typeof Humadity)
         const data = new ValuesModel(req.body)
         await data.save()
-        res.json({Message:"Data Added Success",data})
-    }catch (error) {res.json({Message:"Error",error})}
+        res.json({ Message: "Data Added Success", data })
+    } catch (error) { res.json({ Message: "Error", error }) }
 }
 
-const AddData_Query = async(req,res)=>{
+const AddData_Query = async (req, res) => {
     let Temperature_C2 = req.query.temp
+    let Temperature_F2 = req.query.tempf
     let Humadity2 = req.query.hum
-    let Temperature_F = 1.8*Temperature_C2+32
+
     var Temperature_C = Number(Temperature_C2)
+    var Temperature_F = Number(Temperature_F2)
     var Humadity = Number(Humadity2)
+
+    let Day = req.query.aa
+    let Date = req.query.bb
+    let Month = req.query.cc
+    let Year = req.query.dd
+    let Time_Hours = req.query.ee
+    let Time_Minutes = req.query.ff
+    let Time_Seconds = req.query.gg
+
     try {
         // console.log(typeof Temperature_C)
         // console.log(typeof Humadity)
-        const data = new ValuesModel({Temperature_C,Temperature_F,Humadity})
+        const data = new ValuesModel({ Temperature_C, Temperature_F, Humadity, Day, Date, Month, Year, Time_Hours, Time_Minutes, Time_Seconds })
         await data.save()
-        res.json({Message:"Temp Added Success",data})
-    }catch (error) {res.json({Message:"Error",error})}
+        res.json({ Message: "Temp Added Success", data })
+    } catch (error) { res.json({ Message: "Error", error }) }
 }
 
-const DeleteData = async(req,res)=>{
+const DeleteData = async (req, res) => {
     let _id = req.params.id
     try {
         const data = await ValuesModel.findByIdAndDelete(_id)
-        res.json({Message:"Data Deleted Success",data})
-    }catch (error) {res.json({Message:"Error",error})}
+        res.json({ Message: "Data Deleted Success", data })
+    } catch (error) { res.json({ Message: "Error", error }) }
 }
 
-const UpdateData = async(req,res)=>{
+const UpdateData = async (req, res) => {
     let _id = req.params.id
-    let {Temperature_C,Temperature_F,Humadity} = req.body
+    let { Temperature_C, Temperature_F, Humadity } = req.body
     try {
-        const data = await ValuesModel.findByIdAndUpdate(_id,{Temperature_C,Temperature_F,Humadity})
-        res.json({Message:"Data Updated Success",data})
-    }catch (error) {res.json({Message:"Error",error})}
+        const data = await ValuesModel.findByIdAndUpdate(_id, { Temperature_C, Temperature_F, Humadity })
+        res.json({ Message: "Data Updated Success", data })
+    } catch (error) { res.json({ Message: "Error", error }) }
 }
 
 //......................... APIs ........................
-app.post("/",AddData)  // Adding data through post metheod & body
-app.get("/",GetData)
-app.get("/find/:id",FindData)
-app.get("/add",AddData_Query) // Adding data through get method & query
-app.delete("/delete/:id",DeleteData)
-app.put("/update/:id",UpdateData)
+app.post("/", AddData)  // Adding data through post metheod & body
+app.get("/", GetData)
+app.get("/find/:id", FindData)
+app.get("/add", AddData_Query) // Adding data through get method & query
+app.delete("/delete/:id", DeleteData)
+app.put("/update/:id", UpdateData)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/models.js
+++ models/models.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 const ValuesSchema = new mongoose.Schema({
-    Temperature_C : {},
-    Temperature_F : {},
+    Temperature_C: {},
+    Temperature_F: {},
     Humadity: {},
+    Day: {},
+    Date: {},
+    Month: {},
+    Year: {},
+    Time_Hours: {},
+    Time_Minutes: {},
+    Time_Seconds: {},
 },
-{
-    timestamps:true
-})
- const ValuesModel = mongoose.model("Values",ValuesSchema)
- module.exports = ValuesModel
+    {
+        timestamps: true
+    })
+const ValuesModel = mongoose.model("Values", ValuesSchema)
+module.exports = ValuesModel
